93c4d98fea943be2f40f23419f08e1c437aeeb56
dicksoneverave <44307144+dicksoneverave@users.noreply.github.com>
2025-07-31 21:07:44 +1000

    Deleted create_injury_case_checklist_view.sql

--- supabase/migrations/create_injury_case_checklist_view.sql
+++
@@ -1,9 +0,0 @@
--- Create view for injury case checklist
-CREATE OR REPLACE VIEW InjuryCaseCheckList AS
-SELECT
-  iccl."IRN",
-  iccl."ICCLCriteria",
-  iccl."ICCLFactor",
-  iccl."ICCLDoctorPercentage",
-  iccl."ICCLCompensationAmount"
-FROM "injurycasechecklist" iccl;
