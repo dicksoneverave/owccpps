7ae909ae5e40751572b1345272d97a86b0ce996d
dicksoneverave <44307144+dicksoneverave@users.noreply.github.com>
2025-08-01 23:03:43 +1000

    Updated PrintHearingSetList.tsx

--- src/components/forms/PrintHearingSetList.tsx
+++ src/components/forms/PrintHearingSetList.tsx
@@ -38,6 +38,7 @@ const PrintHearingSetList: React.FC<PrintSetListAllProps> = ({ onClose }) => {
       const { data: publicData, error: publicError } = await supabase
         .from('view_hearings_pending_public')
         .select('*')
+        .eq('THSHearingStatus', 'HearingSet')
         .eq('THSSetForHearing', 'Scheduled');
 
       if (publicError) throw publicError;
@@ -45,6 +46,7 @@ const PrintHearingSetList: React.FC<PrintSetListAllProps> = ({ onClose }) => {
       const { data: privateData, error: privateError } = await supabase
         .from('view_hearings_pending_private')
         .select('*')
+         .eq('THSHearingStatus', 'HearingSet')
         .eq('THSSetForHearing', 'Scheduled');
 
       if (privateError) throw privateError;
